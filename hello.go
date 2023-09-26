b5d6437567386ad246815c6401fe008a3f2e1de4
Runek00 <runek00@gmail.com>
2023-09-26 21:35:26 +0200

    goroutines!

--- hello.go
+++ hello.go
@@ -1,13 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
+	wg := new(sync.WaitGroup)
 	fmt.Println("Hello, World!")
-	fmt.Println(SumMultiples(10))
-	fmt.Println(FindPrimes(20))
-	fmt.Println(IsPalindrome("racecar"))
-	fmt.Println(IsPalindrome("hello"))
-	fmt.Println(Fibonacci(7))
-	fmt.Println(ReverseLinkedList(example(3)).toStr())
+	wg.Add(1)
+	go func() {
+		x := SumMultiples(10)
+		fmt.Println("SumMultiples(10): ", x)
+		wg.Done()
+	}()
+	wg.Add(1)
+	go func() {
+		x := FindPrimes(20)
+		fmt.Println("FindPrimes(20): ", x)
+		wg.Done()
+	}()
+	wg.Add(1)
+	go func() {
+		x := IsPalindrome("racecar")
+		fmt.Println("IsPalindrome(\"racecar\"): ", x)
+		wg.Done()
+	}()
+	wg.Add(1)
+	go func() {
+		x := IsPalindrome("hello")
+		fmt.Println("IsPalindrome(\"hello\"): ", x)
+		wg.Done()
+	}()
+	wg.Add(1)
+	go func() {
+		x := Fibonacci(7)
+		fmt.Println("Fibonacci(7): ", x)
+		wg.Done()
+	}()
+	wg.Add(1)
+	go func() {
+		x := ReverseLinkedList(example(3)).toStr()
+		fmt.Println("ReverseLinkedList(example(3)): ", x)
+		wg.Done()
+	}()
+	wg.Wait()
 }
